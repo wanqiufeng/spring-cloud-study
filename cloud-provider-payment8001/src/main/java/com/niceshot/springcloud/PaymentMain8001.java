ba95850a3b9dd2e14c50dc02672fc5b447b980fd
niceshot <wanqiufeng@outlook.com>
2020-11-11 22:47:12 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/niceshot/springcloud/PaymentMain8001.java
@@ -0,0 +1,10 @@
+package com.niceshot.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
